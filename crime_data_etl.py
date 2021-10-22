8aab35aa8ad827af84ec7c9882e4ad473fa90a76
Iyanumanuel <e.iyanuloluwa@gmail.com>
2021-10-22 16:26:36 +0100

    updated etl files

--- crime_data_etl.py
+++ crime_data_etl.py
@@ -2,29 +2,25 @@
 import pandas as pd
 import sqlalchemy
 import json
+from datetime import datetime
 from sqlalchemy import create_engine
 from decouple import config
 
+#database configuration
 db_conn = config('conn', default= '')
 data_url = config('data_url', default= '')
-
 engine = create_engine(db_conn)
 
+#data source
 url = data_url
-
 json_data = pd.read_json(url)
 
-column_list = ['id', 'case_number', 'date', 'block', 'iucr', 'primary_type',
-       'description', 'location_description', 'arrest', 'domestic', 'beat',
-       'district', 'ward', '1munity_area', 'fbi_code', 'x_coordinate',
-       'y_coordinate', 'year', 'updated_on', 'latitude', 'longitude',
-       'location', 'computed_region_awaf_s7ux', 'computed_region_6mkv_f3dw',
-       'computed_region_vrxf_vc4k', 'computed_region_bdys_3d7i',
-       'computed_region_43wa_7qmu', 'computed_region_rpca_8um6',
-       'computed_region_d9mm_jgwp', 'computed_region_d3ds_rm58']
+#dropping unused columns
+json_data = json_data[json_data.columns.drop(list(json_data.filter(regex='@computed')))].drop(columns= 'location')
        
-json_data.columns = column_list
+#adding a load_time field to track changes
+json_data['load_time'] = datetime.now()
 
-json_data = json_data.astype(str)
-
+#loading to sql database
 json_data.to_sql('crime_data', engine,  if_exists= 'replace')
+print('Successfully sent to sql db at', datetime.now())
